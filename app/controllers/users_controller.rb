e93dbee373c9f94cd86013f5a61211b1c974ea62
Sanya Alekseev <sania2991@rambler.ru>
2017-08-28 20:10:42 +0300

    finished 7.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
+    #debugger   #останавл. программу, открыв. консоль, cnrl-D - выход.
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    #VS @user = User.new(params[:user])   # не окончательная реализация!
+    @user = User.new(user_params)
+    if @user.save
+      # Обработать успешное сохранение
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
