d7edaf89d954a3671c5ab5c8ce9591beb0925d13
Sanya Alekseev <sania2991@rambler.ru>
2017-09-05 18:49:40 +0300

    finished Chapter_8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
