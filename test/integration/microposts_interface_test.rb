354c645294a7e03705ec94d1ad5baa05e92c0d7c
Alexandr <sania2991@rambler.ru>
2017-09-13 20:20:51 +0300

    finished Chapter_11 and Exercises

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,6 +10,8 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    # p_515. Проверка кнопки
+    assert_select 'input[type=submit]'
     # Недопустимая инфа
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: " " } }
@@ -17,9 +19,12 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Допустимое представление
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content,
+                                                   picture: picture } }
     end
+    assert assigns(:current_user).microposts.first.picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
@@ -32,6 +37,21 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # Переход в профиль другого пользователя
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  # Счетчик мкс. в боковой панели
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_url
+    assert_match "#{@user.microposts.count} microposts", response.body
+    # У пользователя нет сообщений
+    other_user = users(:mallory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A microposts")
+    get root_path
+    assert_match "1 micropost", response.body
   end
 
 end
